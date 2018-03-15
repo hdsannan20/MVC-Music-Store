468831c7456102f705dd7cbf2b721ee6c94791d8
hdsannan20 <hdsannan200@hotmail.com>
2018-03-14 21:35:34 -0400

    adding services view

---
+++ MVC-Music-Store/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Music_Store.Startup))]
+namespace MVC_Music_Store
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
