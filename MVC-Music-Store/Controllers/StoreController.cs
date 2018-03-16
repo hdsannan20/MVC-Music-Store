5f7aad72b1e9930f6b547435c29ee98a4eb4b104
hdsannan20 <hdsannan200@hotmail.com>
2018-03-16 17:22:07 -0400

    adding store controller

---
+++ MVC-Music-Store/Controllers/StoreController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Music_Store.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Store/Details?genre=[value]
+        public ActionResult Details(string genre)
+        {
+            // check for an empty genre
+            if (genre == null)
+            {
+                //return View("Error");
+                return RedirectToAction("Index");
+
+            }
+            ViewBag.Genre = genre;
+            return View();
+        }
+    }
+}
